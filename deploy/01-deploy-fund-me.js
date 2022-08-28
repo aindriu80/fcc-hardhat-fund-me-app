361436abfcf44f9e216c2bca0f7b4d4348304f24
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-08-28 12:40:58 +0100

    Hardhat Deploy

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,19 @@
+// import
+
+//main function
+
+// calling of main function
+
+// function deployFunc(hre){
+// 	hre.getNamedAccounts()
+// 	hre.deployments
+// 	console.log("Hello from the initial deploy function")
+// }
+// module.exports.default = deployFunc
+
+module.exports  = async ({getNamedAccounts, deployments}) => {
+	console.log("Hello from the initial deploy function - different syntax")
+	const {deploy, log} = deployments
+	const {deployer} = await getNamedAccounts()
+	const chainId = network.config.chainId
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,33 @@
+// require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+// require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.8",
+  networks:{
+    ropsten:{
+      url:process.env.ROPSTEN_URL || "",
+      accounts:[],
+    },
+  },
+  gasReporter:{
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan:{
+    apiKey: process.env.EtherscanAPI_KEY,
+  },
+  namedAcccounts:{
+    deployer:{
+      default: 0,
+    },
+    users:{
+      default:1,
+    }
+  }
 };
