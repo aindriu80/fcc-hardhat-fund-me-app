83f12d822be85b931b3e80c7ebdd60ea388a7b91
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-09-08 20:59:50 +0100

    Cheaper withdrawal initial

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -136,5 +136,52 @@ describe("FundMe", async function() {
                 // ).to.be.revertedWith("FundMe__NotOwner")
             ).to.be.revertedWithCustomError(fundMe, "FundMe__NotOwner")
         })
+        it("cheaperWithdraw testing....", async function() {
+            // Arrange
+            const accounts = await ethers.getSigners()
+            for (let i = 0; i < 6; i++) {
+                const fundMeConnectedContract = await fundMe.connect(
+                    accounts[i]
+                )
+                await fundMeConnectedContract.fund({
+                    value: sendValue
+                })
+            }
+            const startingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const startingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+
+            // Act
+            const transactionResponse = await fundMe.cheaperWithdraw()
+            const transactionReceipt = await transactionResponse.wait(1)
+            const { gasUsed, effectiveGasPrice } = transactionReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+
+            // Assert
+            const endingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const endingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+            assert.equal(endingFundMeBalance, 0)
+            assert.equal(
+                startingDeployerBalance.add(startingFundMeBalance).toString(),
+                // startingFundMeBalance.add(startingDeployerBalance),
+                endingDeployerBalance.add(gasCost).toString()
+            )
+
+            // Make sure that the funders are reset properly
+            await expect(fundMe.getFunder(0)).to.be.reverted
+            for (i = 1; i < 6; i++) {
+                assert.equal(
+                    await fundMe.getAddressToAmountFunded(accounts[i].address),
+                    0
+                )
+            }
+        })
     })
 })
